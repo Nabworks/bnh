e775647d9457196153193813583e85e598485c52
Suijin <70591379+Cyberkingcr7@users.noreply.github.com>
2021-11-01 09:56:52 +0200

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -25,7 +25,7 @@ export default class Command extends BaseCommand {
                     user.notify || user.vname || user.name || 'null'
                 }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
-        text += `\nTo deploy your own Bot or To support Kaoi👾\nVisit : https://github.com/cyberkingcr7/bnh `
+        text += `\nTo deploy your own Bot or To support Bnh👾\nVisit : https://github.com/cyberkingcr7/bnh `
         return void M.reply(text)
     }
 }
