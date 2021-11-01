a3d9c732d2f7cbffd2df5d2e1748712cdbea5faa
Suijin <70591379+Cyberkingcr7@users.noreply.github.com>
2021-11-01 09:03:35 +0200

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => {
             const n = [
-            './assets/images/help/help.mp4'
+            './assets/images/help.mp4'
         ]
         let bnh = n[Math.floor(Math.random() * n.length)]
         if (!parsedArgs.joined) {
