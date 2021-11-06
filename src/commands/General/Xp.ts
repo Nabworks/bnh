d62834e76ebe3e64d8304138370d6e37adf5475b
Suijin <70591379+Cyberkingcr7@users.noreply.github.com>
2021-11-06 17:00:14 +0200

    Update Xp.ts

--- src/commands/General/Xp.ts
+++ src/commands/General/Xp.ts
@@ -24,6 +24,7 @@ export default class Command extends BaseCommand {
             // username = contact.notify || contact.vname || contact.name || user.split('@')[0]
             username = user.split('@')[0]
         }
-        return void (await M.reply(`${username} XP: ${(await this.client.getUser(user)).Wallet || 0}`))
+        return void (await M.reply(`${username} XP: ${(await this.client.getUser(user)).Xp
+                                  || 0}`))
     }
 }
